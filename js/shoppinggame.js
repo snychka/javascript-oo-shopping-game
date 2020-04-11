9ca5a0628a7da65d346219990dbd502d797e637e
snychka <stefannychka@gmail.com>
2020-04-11 11:07:37 -0600

    mod4, Points, cannot pass last task

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -264,8 +264,14 @@ const findPointsForExpDate = (prod) => {
 
 
 const calculatePoints = (prod, tBill) => {
-    let pointsToBill = findPointsToBill(Math.round(tBill));
-    let pointsForExpDate = findPointsForExpDate(prod);
+  let pointsToBill = findPointsToBill(Math.round(tBill));
+  let pointsForExpDate = findPointsForExpDate(prod);
+  let score = player.score + pointsToBill + pointsForExpDate;
+
+  if (prod instanceof MagicProduct) {
+    if (prod.isBonus) {player.addPoints(prod.points);}
+    else {player.deductPoints(prod.points);}
+  }
 };
 
 // Complete this function
